8dfee1be9f1a1762f1fe97a01e92f98e919fb842
DANKAMN <danielnicholas1477@gmail.com>
2024-03-15 07:13:54 +0100

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+  return (
+    <div className="bg-orange-500 py-10">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <span className="text-3xl text-white tracking-tighter">
+                Daniel Eats
+            </span>
+            <span className="text-white track-tight flex gap-4">
+                <span>© 2024 Daniel Technologies Inc.</span>
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
